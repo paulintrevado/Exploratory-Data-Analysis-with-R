4af78e6ab5bf87c0dbac44dd7a3e2084b0c2eb81
Paul Intrevado <pintrevado@usfca.edu>
2017-10-22 14:34:50 -0700

    Add files via upload

---
+++ courseCode/class01intro.R
@@ -0,0 +1,12 @@
+age <- c(23, 35, 31, 49, 77, 42, 50, 30, 12)
+salaryInThousands <- c(33, 105, 153, 0, 74, 99, 32, 188, 12)
+
+mean(age)
+sd(age)
+var(salaryInThousands)
+cor(age,salaryInThousands)
+hist(age)
+hist(salaryInThousands)
+plot(age,salaryInThousands)
+
+
